bb57005e9ca230f98af7133fa2f9f39661b2eb40
iDigitalFlame <idf@idigitalflame.com>
2025-01-30 16:12:33 -0500

    Updated Install Scripts and added Python cache

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# Copyright 2021 - 2024 iDigitalFlame
+# Copyright 2021 - 2025 iDigitalFlame
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -177,6 +177,10 @@ setup_config() {
     mkdir -p "${SETUP_ROOT}/var/cache/pacman"
     mkdir -p "${SETUP_ROOT}/var/cache/pacman/pkg"
     chmod 0555 "${SETUP_ROOT}/var/db/pacman"
+
+    mkdir "${SETUP_ROOT}/var/cache/python"
+    chmod 3777 "${SETUP_ROOT}/var/cache/python"
+    chown root:root "${SETUP_ROOT}/var/cache/python"
 
     mv "${SETUP_ROOT}/etc/ld.so.cache" "${SETUP_ROOT}/var/cache/ld.so.cache"
     mv "${SETUP_ROOT}/etc/pacman.d/mirrorlist" "${SETUP_ROOT}/var/cache/pacman/mirrorlist"
